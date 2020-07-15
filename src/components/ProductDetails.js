129588ebb9a59dea63131e154aeb980b5bfe15b2
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-07-15 16:04:14 +0500

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -10,7 +10,7 @@ export const ProductDetails = () => {
         return <h2>Product Not Found...</h2>
     return (
         <div>
-            <h1>Product Details Page</h1>
+            <h1>This is Product Details Page</h1>
             <div className="productDetail animate__animated animate__zoomIn"> 
                 <h4>{shoe.name}</h4>
                 <img src={shoe.img} height={500} alt="Shoe-Name"></img>
