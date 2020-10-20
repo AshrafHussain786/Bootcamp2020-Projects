2b10f18dff47e2d19b8b6cdf3bf1be76836d47e1
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-10-20 12:20:52 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,19 +11,19 @@ export function Contact() {
             preserveAspectRatio: "xMidYMid slice"
         }
     };
-    
+
     return (
         <>
             <div id="contact" style={{
                 display: "flex",
                 justifyContent: "center",
-                marginTop: "100px",                
+                marginTop: "100px",
             }}>
                 <div>
                     <Lottie
                         options={defaultOptions}
                         height={"80%"}
-                        width={"300%"}                        
+                        width={"300%"}
                     />
                 </div>
                 <div>
@@ -31,39 +31,37 @@ export function Contact() {
                         <p className="text-center"> If you have any query, please send me an email....</p>
                     </div>
                     <div className="container-fluid mb-5">
-                        {/* <div className="row"> */}
-                            <div className="col-md-12 col-10 mx-auto">
-                               <form action="mailto: ashrafhussain786@yahoo.com" className="needs-validation" novalidate method="post" encType="text/plain">
-                                    <div className="form-group">
-                                        <label htmlFor="uname">Name</label>
-                                        <input type="text" name='uname' onChange={(e) => (e.target.value)} required className="form-control" placeholder="Enter you full name" />
-                                        <div class="valid-feedback">Valid.</div>
-                                        <div class="invalid-feedback">Please fill out this field.</div>
-                                    </div>
-                                    <div>
-                                        <label htmlFor="phone">Phone Number</label>
-                                        <input type="number" name='phone' className="form-control" onChange={(e) => (Number(e.target.value))} placeholder="Enter you phone number" />
-                                        <div class="valid-feedback">Valid.</div>
-                                        <div class="invalid-feedback">Please fill out this field.</div>
-                                    </div>
-                                    <div>
-                                        <label htmlFor="email">Email address</label>
-                                        <input type="email" name='email' required className="form-control" onChange={(e) => (e.target.value)} placeholder="Enter you email address" />
-                                        <div class="valid-feedback">Valid.</div>
-                                        <div class="invalid-feedback">Please fill out this field.</div>
-                                    </div>
-                                    <div>    
-                                        <label htmlFor="msg">Message</label>
-                                        <textarea required className="form-control" rows={5} name='msg' onChange={(e) => (e.target.value)}  ></textarea>
-                                        <br /> <br />
-                                        <button type="submit" className="btn btn-primary mb-2">Submit</button>
-                                    </div>
-                                </form>
-                            </div>
+                        <div className="col-md-12 col-10 mx-auto">
+                            <form action="mailto: ashrafhussain786@yahoo.com" className="needs-validation" novalidate method="post" encType="text/plain">
+                                <div className="form-group">
+                                    <label htmlFor="uname">Name</label>
+                                    <input type="text" name='uname' onChange={(e) => (e.target.value)} required className="form-control" placeholder="Enter you full name" />
+                                    <div class="valid-feedback">Valid.</div>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">Phone Number</label>
+                                    <input type="number" name='phone' className="form-control" onChange={(e) => (Number(e.target.value))} placeholder="Enter you phone number" />
+                                    <div class="valid-feedback">Valid.</div>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">Email address</label>
+                                    <input type="email" name='email' required className="form-control" onChange={(e) => (e.target.value)} placeholder="Enter you email address" />
+                                    <div class="valid-feedback">Valid.</div>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                                <div>
+                                    <label htmlFor="msg">Message</label>
+                                    <textarea required className="form-control" rows={5} name='msg' onChange={(e) => (e.target.value)}  ></textarea>
+                                    <br /> <br />
+                                    <button type="submit" className="btn btn-primary mb-2">Submit</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            {/* </div> */}
+            </div>
         </>
     );
 }
