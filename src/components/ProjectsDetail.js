1694581319b63178337c9aa1071f366c16e04314
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-11-05 07:08:39 +0500

    project detail file updated

--- src/components/ProjectsDetail.js
+++ src/components/ProjectsDetail.js
@@ -1,18 +1,10 @@
-import Project1 from "../images/bootcamp.jpg";
-import Project2 from "../images/bootcamp.jpg";
-import Project3 from "../images/bootcamp.jpg";
-import Project4A from "../images/bootcamp.jpg";
-import Project4B from "../images/bootcamp.jpg";
-import Project5 from "../images/bootcamp.jpg";
-import Project6 from "../images/bootcamp.jpg";
-import Project7A from "../images/bootcamp.jpg";
-import Project7B from "../images/bootcamp.jpg";
-import Project8 from "../images/bootcamp.jpg";
+import Bootcamp from "../images/bootcamp.jpg";
+
 
 export const ProjectsDetail = [
     {
         id: 1,
-        imgsrc: Project1,
+        imgsrc: Bootcamp,
         projectId: "Project-1",
         title: " Expense Tracker App",
         projectlink:"http://bootcamp-tracker-ashraf.surge.sh/",
@@ -20,7 +12,7 @@ export const ProjectsDetail = [
     },
     {
         id: 2,
-        imgsrc: Project2,
+        imgsrc: Bootcamp,
         projectId: "Project-2",
         title: "Covid-19 Tracker App",
         projectlink:"http://bootcamp-covid19react-ashraf.surge.sh/",
@@ -28,7 +20,7 @@ export const ProjectsDetail = [
     },
     {
         id: 3,
-        imgsrc: Project3,
+        imgsrc: Bootcamp,
         projectId: "Project-3",
         title: "Shoe Store",
         projectlink:"http://bootcamp-shoe-store-ashraf.surge.sh/",
@@ -36,7 +28,7 @@ export const ProjectsDetail = [
     },
     {
         id: 4,
-        imgsrc: Project4A,
+        imgsrc: Bootcamp,
         projectId: "Project-4A",
         title: "Red Queen’s Race",
         projectlink:"http://bootcamp-project4a-ashraf.surge.sh/",
@@ -44,7 +36,7 @@ export const ProjectsDetail = [
     },
     {
         id: 5,
-        imgsrc: Project4B,
+        imgsrc: Bootcamp,
         projectId: "Project-4B",
         title: "Tiny Short Application",
         projectlink:"http://bootcamp-project4b-ashraf.surge.sh/",
@@ -52,7 +44,7 @@ export const ProjectsDetail = [
     },
     {
         id: 6,
-        imgsrc: Project5,
+        imgsrc: Bootcamp,
         projectId: "Project-5",
         title: "Animated React Website",
         projectlink:"http://bootcamp-project5-ashraf.surge.sh/",
@@ -60,23 +52,23 @@ export const ProjectsDetail = [
     },
     {
         id: 7,
-        imgsrc: Project6,
+        imgsrc: Bootcamp,
         projectId: "Project-6",
         title: " Quiz App",
         projectlink:"http://bootcamp-project6-ashraf.surge.sh/",
         gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-6"        
     },
-    {
-        id: 8,
-        imgsrc: Project7A,
-        projectId: "Project-7A",
-        title: "Progressive Web App",
-        projectlink:"In process",
-        gitlink: "In process"        
-    },
+    // {
+    //     id: 8,
+    //     imgsrc: Bootcamp,
+    //     projectId: "Project-7A",
+    //     title: "Progressive Web App",
+    //     projectlink:"In process",
+    //     gitlink: "In process"        
+    // },
     {
         id: 9,
-        imgsrc: Project7B,
+        imgsrc: Bootcamp,
         projectId: "Project-7B",
         title: "React Timer App",
         projectlink:"http://bootcamp-project7b-ashraf.surge.sh/",
@@ -84,10 +76,18 @@ export const ProjectsDetail = [
     },
     {
         id: 10,
-        imgsrc: Project8,
+        imgsrc: Bootcamp,
         projectId: "Project-8",
         title: "SpaceX GraphQL TypeScript React App",
         projectlink:"http://bootcamp-spacex-ashraf.surge.sh/",
         gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-8"        
     },
+    {
+        id: 11,
+        imgsrc: Bootcamp,
+        projectId: "Project-9A",
+        title: "Shopping Basket",
+        projectlink:"http://bootcamp-project9a-ashraf.surge.sh/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-9A"        
+    },    
 ]
