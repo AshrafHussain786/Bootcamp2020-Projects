b599e47accfc16de306b3327b69eaf80eca57c25
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-11-28 14:15:35 +0500

    project 9B added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import * as serviceWorker from "./serviceWorker";
+import { setupServer } from './services/mirage/server';
+import { Provider } from 'react-redux';
+import store from './store';
+
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
