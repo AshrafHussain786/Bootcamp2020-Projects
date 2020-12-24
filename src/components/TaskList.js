a8b7f49386ce150d1cfadf2c2ec390299b54c751
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-12-25 00:07:17 +0500

    updated files

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import Task from './Task';
-import "./TaskList.css"
+import PropTypes from 'prop-types';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import Task from './Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
+
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -17,9 +19,10 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
       </span>
     </div>
   );
+
   if (loading) {
     return (
-      <div className="list-items">
+    <div className="list-items">
         {LoadingRow}
         {LoadingRow}
         {LoadingRow}
@@ -29,10 +32,11 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
       </div>
     );
   }
+
   if (tasks.length === 0) {
     return (
-      <div className="list-items">
-        <div className="wrapper-message">
+       <div className="list-items">
+         <div className="wrapper-message">
           <span className="icon-check" />
           <div className="title-message">You have no tasks</div>
           <div className="subtitle-message">Sit back and relax</div>
@@ -40,10 +44,12 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
       </div>
     );
   }
+
   const tasksInOrder = [
     ...tasks.filter(t => t.state === 'TASK_PINNED'),
     ...tasks.filter(t => t.state !== 'TASK_PINNED'),
   ];
+
   return (
     <div className="list-items">
       {tasksInOrder.map(task => (
@@ -53,16 +59,27 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
-    /** Checks if it's in loading state */
-    loading: PropTypes.bool,
-    /** The list of tasks */
-    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-    /** Event to change the task to pinned */
-    onPinTask: PropTypes.func,
-    /** Event to change the task to archived */
-    onArchiveTask: PropTypes.func,
-  };
-  TaskList.defaultProps = {
-    loading: false,
-  };
+PureTaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func.isRequired,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func.isRequired,
+};
+
+PureTaskList.defaultProps = {
+  loading: false,
+};
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
