69a589f357f40382aa3fb0462f4f76c69c9b0e06
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2021-01-21 22:59:51 +0500

    Updated Projects

--- src/components/ProjectsDetail.js
+++ src/components/ProjectsDetail.js
@@ -58,14 +58,14 @@ export const ProjectsDetail = [
         projectlink:"http://bootcamp-project6-ashraf.surge.sh/",
         gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-6"        
     },
-    // {
-    //     id: 8,
-    //     imgsrc: Bootcamp,
-    //     projectId: "Project-7A",
-    //     title: "Progressive Web App",
-    //     projectlink:"In process",
-    //     gitlink: "In process"        
-    // },
+    {
+        id: 8,
+        imgsrc: Bootcamp,
+        projectId: "Project-7A",
+        title: "Progressive Web App",
+        projectlink:"http://bootcamp-project7a-ashraf.surge.sh/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-7A"        
+    },
     {
         id: 9,
         imgsrc: Bootcamp,
@@ -89,5 +89,69 @@ export const ProjectsDetail = [
         title: "Shopping Basket",
         projectlink:"http://bootcamp-project9a-ashraf.surge.sh/",
         gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-9A"        
+    }, 
+    {
+        id: 12,
+        imgsrc: Bootcamp,
+        projectId: "Project-9B",
+        title: "Diaries App using React Redux Toolkit",
+        projectlink:"http://bootcamp-project9b-ashraf.surge.sh/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-9B"        
+    },
+    {
+        id: 13,
+        imgsrc: Bootcamp,
+        projectId: "Project-10",
+        title: "Multi-step Form with Formik and Yup",
+        projectlink:"http://bootcamp-project10-ashraf.surge.sh/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-10"        
     },    
+    {
+        id: 14,
+        imgsrc: Bootcamp,
+        projectId: "Project-11",
+        title: "Taskbox using Storybook",
+        projectlink:"http://bootcamp-project11-ashraf.surge.sh/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-11"        
+    },
+    {
+        id: 15,
+        imgsrc: Bootcamp,
+        projectId: "Project-12A",
+        title: "Blog site in Gatsby & Contentful and Deploy on Netlify",
+        projectlink:"https://bootcamp-project12a-ashraf.netlify.app/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-12A"        
+    },
+    {
+        id: 16,
+        imgsrc: Bootcamp,
+        projectId: "Project-12B",
+        title: "Serverless CRUD apps with Netlify Functions and FaunaDB",
+        projectlink:"https://bootcamp-project12a-ashraf.netlify.app/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-12B"        
+    },
+    {
+        id: 17,
+        imgsrc: Bootcamp,
+        projectId: "Project-12C",
+        title: "Serverless JAMStack Todo app with Netlify, Gatsby, GraphQL, and FaunaDB",
+        projectlink:"https://bootcamp-project12c-ashraf.netlify.app/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-12C"        
+    },
+    {
+        id: 18,
+        imgsrc: Bootcamp,
+        projectId: "Project-12D",
+        title: "Bookmarking Application With FaunaDB, Netlify And Gatsby",
+        projectlink:"https://bootcamp-project12d-ashraf.netlify.app/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-12D"        
+    },
+    {
+        id: 19,
+        imgsrc: Bootcamp,
+        projectId: "Project-12E",
+        title: "Virtual Lolly using Gatsby, Netlify, FaunaDB",
+        projectlink:"https://bootcamp-project12ee-ashraf.netlify.app/",
+        gitlink: "https://github.com/AshrafHussain786/Bootcamp2020-Projects/tree/Project-12E"        
+    },
 ]
