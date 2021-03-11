a65a29c46a886787f6d2472227b4e63b390ab2b4
MuhammadAshraf <ashrafhussain786@yahoo.com>
2021-03-11 23:55:42 +0500

    updated gatsby config & node files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,31 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({ actions, graphql }) => {
-  const result = await graphql(`
-  query MyQuery{
-    LOLLIES{
-    getAllLollies{
-        recipientName
-        senderName
-        message
-        flavourTop
-        flavourMiddle
-        flavourBottom
-        lollyPath
-  }}}
-  `)
+// exports.createPages = async ({ actions, graphql }) => {
+//   const result = await graphql(`
+//   query MyQuery{
+//     Lollies{
+//     getAllLollies{
+//         recipientName
+//         senderName
+//         message
+//         flavourTop
+//         flavourMiddle
+//         flavourBottom
+//         lollyPath
+//   }}}
+//   `)
 
-  console.log("Data from gatsby-node file ===> ", result.data)
-//   result.Lollies.data.getAllLollies.map( async (lollyPath ) => {    
-//     await actions.createPage({
-    data.LOLLIES.getAllLollies.forEach((node) => {
-      actions.createPage({
-      path: `lollies/${node.lollyPath}`,
-      component: path.resolve(`./src/template/lollyPage.jsx`),
-    //   context: {
-    //     lollies: lollyPath,
-    //   },
-      context: node
-    })
-  })
-}
+//   console.log("Data from gatsby-node file ===> ", result.data)
+// //   result.Lollies.data.getAllLollies.map( async (lollyPath ) => {    
+// //     await actions.createPage({
+//     data.LOLLIES.getAllLollies.forEach((node) => {
+//       actions.createPage({
+//       path: `lollies/${node.lollyPath}`,
+//       component: path.resolve(`./src/template/lollyPage.jsx`),
+//     //   context: {
+//     //     lollies: lollyPath,
+//     //   },
+//       context: node
+//     })
+//   })
+// }
